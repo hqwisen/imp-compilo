2f8771776ac386ca421901c2f4e105e12d887e9f
Hakim Boulahya <hakimblhya@gmail.com>
2017-10-30 16:46:18 +0100

    test.imp source file

--- more/src/Symbol.java
+++ more/src/Symbol.java
@@ -1,7 +1,7 @@
 public class Symbol{
 	public static final int UNDEFINED_POSITION = -1;
 	public static final Object NO_VALUE = null;
-
+	
 	private final LexicalUnit type;
 	private final Object value;
 	private final int line,column;
@@ -12,19 +12,19 @@ public class Symbol{
 		this.column	= column;
 		this.value	= value;
 	}
-
+	
 	public Symbol(LexicalUnit unit,int line,int column){
 		this(unit,line,column,NO_VALUE);
 	}
-
+	
 	public Symbol(LexicalUnit unit,int line){
 		this(unit,line,UNDEFINED_POSITION,NO_VALUE);
 	}
-
+	
 	public Symbol(LexicalUnit unit){
 		this(unit,UNDEFINED_POSITION,UNDEFINED_POSITION,NO_VALUE);
 	}
-
+	
 	public Symbol(LexicalUnit unit,Object value){
 		this(unit,UNDEFINED_POSITION,UNDEFINED_POSITION,value);
 	}
@@ -32,34 +32,34 @@ public class Symbol{
 	public boolean isTerminal(){
 		return this.type != null;
 	}
-
+	
 	public boolean isNonTerminal(){
 		return this.type == null;
 	}
-
+	
 	public LexicalUnit getType(){
 		return this.type;
 	}
-
+	
 	public Object getValue(){
 		return this.value;
 	}
-
+	
 	public int getLine(){
 		return this.line;
 	}
-
+	
 	public int getColumn(){
 		return this.column;
 	}
-
+	
 	@Override
 	public int hashCode(){
 		final String value	= this.value != null? this.value.toString() : "null";
 		final String type		= this.type  != null? this.type.toString()  : "null";
 		return new String(value+"_"+type).hashCode();
 	}
-
+	
 	@Override
 	public String toString(){
 		if(this.isTerminal()){
