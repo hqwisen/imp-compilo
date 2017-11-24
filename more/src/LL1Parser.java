503e57bdd43ca424ec1b3a60da4d2c8a4c3b492a
Hakim Boulahya <hakimblhya@gmail.com>
2017-11-24 22:54:40 +0100

    Adding parser gradle tasks and refactor. mains

--- more/src/LL1Parser.java
+++ more/src/LL1Parser.java
@@ -1,5 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class LL1Parser {
 
+    private final static String JAR = "dist/parser.jar";
 
     private String grammarFile, tableFile;
 
@@ -8,5 +12,20 @@ public class LL1Parser {
         this.tableFile = tableFile;
     }
 
+    public static void main(String[] args) {
+        System.out.println("Running parser");
+        if (args.length != 1) {
+            System.out.println("Usage:  java -jar " + JAR + " file.imp\n");
+            System.exit(0);
+        }
+        FileReader source = Scanner.file(args[0]);
+        if(source == null){
+            System.out.println("File '" + args[0] + "' was not found");
+        }else {
+            Scanner scanner = new GeneratedScanner(source);
+            scanner.scan();
+            scanner.postScan();
+        }
+    }
 
 }
